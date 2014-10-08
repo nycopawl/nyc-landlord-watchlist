8d463ef3af5bd93986ac928fddb6a28dda29d6ed
Jeff Lyon <jeff@albatrossdigital.com>
2014-10-08 09:03:03 -0700

    upsate

--- js/directives.js
+++ js/directives.js
@@ -48,10 +48,10 @@ angular.module('map.directives', [])
         
 
         // The visible tile layer
-        L.mapbox.tileLayer('http://maps2.albatrossdigital.com:8888/v2/watchlist3_839858.json').addTo(map);
+        L.mapbox.tileLayer('http://maps2.albatrossdigital.com:8888/v2/watchlist3_ca193d.json').addTo(map);
 
         // Load interactivity data into the map with a gridLayer
-        var gridLayer = L.mapbox.gridLayer('http://maps2.albatrossdigital.com:8888/v2/watchlist3_839858.json');
+        var gridLayer = L.mapbox.gridLayer('http://maps2.albatrossdigital.com:8888/v2/watchlist3_ca193d.json');
         gridLayer.addTo(map);
 
         // And use that interactivity to drive a control the user can see.
